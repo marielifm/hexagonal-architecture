faf77527dcf77e34109a659e5b334490bd045409
marielifm <marieli.matos@hotmail.com>
2024-01-08 15:01:26 -0300

    feat: creating a cli

---
+++ cmd/cli.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/marielifm/hexagonal-architecture/app/adapters/cli"
+	"github.com/spf13/cobra"
+)
+
+var action string
+var productId string
+var productName string
+var productPrice float64
+
+// cliCmd represents the cli command
+var cliCmd = &cobra.Command{
+	Use:   "cli",
+	Short: "This command create, show and update a product",
+	Long:  `This cli manages the creation, update and shows a product.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		res, err := cli.Run(&productService, action, productId, productName, productPrice)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println(res)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(cliCmd)
+
+	cliCmd.Flags().StringVarP(&action, "action", "a", "enable", "Enable / Disable a product")
+	cliCmd.Flags().StringVarP(&productId, "id", "i", "", "Product Id")
+	cliCmd.Flags().StringVarP(&productName, "product", "n", "", "Product Name")
+	cliCmd.Flags().Float64VarP(&productPrice, "price", "p", 0, "Product Price")
+}
--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-	"database/sql"
-
-	"github.com/marielifm/hexagonal-architecture/app"
-	"github.com/marielifm/hexagonal-architecture/app/adapters/db"
-)
+import "github.com/marielifm/hexagonal-architecture/cmd"
 
 func main() {
-	database, _ := sql.Open("sqlite3", "sqlite.db")
-	productDbAdapter := db.NewProductDb(database)
-	productService := app.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product 1", 30)
-	productService.Enable(product)
+	cmd.Execute()
 }
